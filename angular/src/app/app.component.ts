cd7f15ee3dc4879a2e1f062da6137e3a87fb68fc
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-05-16 17:21:43 -0500

    11 components y todo enrutado

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { IniciarSesionComponent } from './components/iniciar.sesion/iniciar.sesion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { PrivadoComponent } from './components/privado/privado.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    ContactoComponent, IniciarSesionComponent, InicioComponent, NosotrosComponent, PrivadoComponent, ProductosComponent, RegistrarseComponent, ServiciosComponent, HeaderComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular';
-}
+export class AppComponent {}
--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from "./components/contacto/contacto.component";
+import { IniciarSesionComponent } from "./components/iniciar.sesion/iniciar.sesion.component";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { NosotrosComponent } from "./components/nosotros/nosotros.component";
+import { PrivadoComponent } from "./components/privado/privado.component";
+import { ProductosComponent } from "./components/productos/productos.component";
+import { RegistrarseComponent } from "./components/registrarse/registrarse.component";
+import { ServiciosComponent } from "./components/servicios/servicios.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'contacto', component: ContactoComponent},
+    {path : 'iniciar.sesion', component: IniciarSesionComponent},
+    {path : 'inicio', component: InicioComponent},
+    {path : 'nosotros', component: NosotrosComponent},
+    {path : 'privado', component: PrivadoComponent},
+    {path : 'productos', component: ProductosComponent},
+    {path : 'registrarse', component: RegistrarseComponent},
+    {path : 'servicios', component: ServiciosComponent}
+];
